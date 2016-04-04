46f7472936e9fc230563f9c79dea829e544df66c
Luke Thompson <luke@yorkbay.net.nz>
2016-04-04 21:40:37 +1200

    Updated

--- Main.elm
+++ Main.elm
@@ -4,9 +4,7 @@ import Graphics.Collage exposing (..)
 import Graphics.Element exposing (..)
 import Keyboard exposing (arrows, keysDown)
 import Signal exposing (foldp)
-import Char exposing (fromCode)
 import Time exposing (fps)
-import String
 
 main : Signal Element
 main = Signal.map view (foldp update init updates)
@@ -16,22 +14,34 @@ type alias State =
   , moving : Float
   }
 
+init : State
 init = { moving = 0.0, carx = 0.0 }
 
---update : Update -> State -> State
+update : Update -> State -> State
 update up st =
   case up of 
-    TimeDelta t -> let c = st.carx in { st | carx = c + st.moving * 1 } 
-    LeftArrow i -> {st | moving = toFloat i }
+    TimeDelta _ -> let c = st.carx in { st | carx = updateX c st.moving } 
+    SideArrow i -> {st | moving = toFloat i }
 
-type Update = TimeDelta Float | LeftArrow Int 
+updateX : Float -> Float -> Float
+updateX x d = 
+    let x' = x + d * 3
+    in  if x' > 410.0
+          then -410.0
+          else if x' < -410.0
+                 then 410.0 
+                 else x'
 
-leftArrow : Signal Int
-leftArrow = Signal.map (.x) arrows
 
+type Update = TimeDelta Float | SideArrow Int
+
+sideArrow : Signal Int
+sideArrow = Signal.map (.x) arrows
+
+updates : Signal Update
 updates = Signal.merge 
-              (Signal.map LeftArrow leftArrow)
-              (Signal.map TimeDelta (fps 400))
+              (Signal.map SideArrow sideArrow)
+              (Signal.map TimeDelta (fps 100))
 
 view : State -> Element
 view st = header `above` (board st.carx)
@@ -43,14 +53,14 @@ header =
             |> Text.color red           
             |> Text.height 40
             |> Text.bold
-  in container 1500 100 middle (centered h)
+  in container 1200 100 middle (centered h)
 
 -------------------------------------------------------
 -- Board
 -------------------------------------------------------
 
 board : Float -> Element
-board carx = collage 1500 500 
+board carx = collage 1200 500 
           [ rect 800 500 |> filled clearGrey
           , car |> move (carx, -200) ] 
 
@@ -59,14 +69,7 @@ board carx = collage 1500 500
 --------------------------------
 
 car : Form
-car = 
-  group
-    [ filled carBlue (oval 40 75)
-    , filled carBlack (rect 15 15) |> move (25, 25)
-    , filled carBlack (rect 15 15) |> move (-25, 25)
-    , filled carBlack (rect 15 15) |> move (25, -25)
-    , filled carBlack (rect 15 15) |> move (-25, -25)
-    ]
+car = toForm <| image 50 75 "sprites/car_number_one.png"
 
 
 clearGrey : Color
